6d6a0c9eed1f193b6a670778a8e8c66c54ac7fcb
Prashant Gupta <prashant.gupta@tothenew.com>
2017-03-07 20:57:43 +0530

    Final Project

--- Project/script.js
+++ Project/script.js
@@ -1,4 +1,3 @@
-
 	  	$(document).ready(function(){
 			
 			var note2000 = 0, note500 = 0, note100 = 0, withdrawAmount;
@@ -15,12 +14,15 @@
 				this.fillATM = function()
 				{
 					var value=$("#max_limit").val();
-           			//if(value % 100 ==0){
-		   
-						o.obj.count_2000 = $("#2000").val();
+					
+           			if(value % 100 !=0 || value == 0 || value < 0) {
+						alert("Enter in multiple of 100");
+						$("#max_limit").focus();
+					}else {
+               			o.obj.count_2000 = $("#2000").val();
 						o.obj.count_500 = $("#500").val();
-						 o.obj.count_100 = $("#100").val();
-						   o.obj.max_limit = $("#max_limit").val();
+						o.obj.count_100 = $("#100").val();
+						o.obj.max_limit = $("#max_limit").val();
 						o.leftAmount = o.obj.count_2000*2000 + o.obj.count_500*500 + o.obj.count_100*100;
 						var str = "<tr id='fillatm'>" +
 						"<td>" + o.leftAmount +"<\/td>" +
@@ -30,21 +32,13 @@
 						"<td>" + o.leftAmount +"<\/td>" +
 						"</tr>";
 						$('#table1').find('tbody').append(str);
-					$('#currentAmount').html(o.leftAmount);
+						$('#currentAmount').html(o.leftAmount);
 						$('#fillatm').css({ 'background-color' : 'green'});
-
-					/*}else
-						{
-							
-               				alert("limit should be multiple of 100");
-						}*/
+						$("#btn1").attr("disabled", true);
+					}
 				}
 
-				
-				
-				
 			}
-            
 			
 			var o = new ATM();
 			
@@ -67,12 +61,9 @@
 			function checkError(){
 					
 				var dailyWithdrawLimit = o.obj.max_limit;
-               
 					
                 withdrawAmount = document.getElementById("amount").value;
-
-                
-
+				
                 if (withdrawAmount % 100 != 0 || withdrawAmount == 0 || withdrawAmount < 0){
 
                     document.getElementById("amount").value = "";
@@ -80,27 +71,20 @@
 
                     var error = "OOPS ! You have Entered Wrong Denominations. Please Enter in Multiples of Hundred."
                     document.getElementById("error").innerHTML = error;
+					$("#amount").focus() ;
                 }
 
-                else {
-                    
-                    withdrawAmount = parseInt(withdrawAmount);
-
-                    
-
-                    if(withdrawAmount > dailyWithdrawLimit){
-
-                        
+                else { 
+					withdrawAmount = parseInt(withdrawAmount);
+                    if(withdrawAmount > dailyWithdrawLimit) {
                         var limitError = "Your Daily Withdrawl Limit is " + dailyWithdrawLimit + " only , Please Re-Enter the Amount" ;
                         document.getElementById("amount").value = "";
                         document.getElementById("currencyDenominations").innerHTML = "";
-
                         document.getElementById("error").innerHTML = limitError;
-
-                    }
-
-                    else{
-
+						$("#amount").focus() ;
+					}
+                    else {
+						note2000 = 0, note500 = 0, note100 = 0;
 						o.withdrawalAmount = withdrawAmount;
                         while (withdrawAmount >= 100)
                         {
@@ -125,49 +109,33 @@
                             }
 							
                         }
-						if(o.obj.count_2000 >= note2000 && o.obj.count_500 >= note500 && o.obj.count_100 >= note100)
-							{
+						if(o.obj.count_2000 >= note2000 && o.obj.count_500 >= note500 && o.obj.count_100 >= note100) {
 								o.obj.count_2000 -= note2000;
 								o.obj.count_500 -= note500;
 								o.obj.count_100 -= note100;
+								document.getElementById("currencyDenominations").innerHTML = "<emp>The Denominations for the above amount will be </emp> <br>  <label> 2000 :- <span>" + note2000 +"</span> </label> <br> <label> 500 :- <span>" + note500 +"</span></label> <br/> <label> 100 :- <span>" + note100 + "</span></label>";
 								addLog();
-							}
-							
-						else
-							document.getElementById("error").innerHTML = "Cannot withdraw Amount";
-						
-													
-						
-						
+							document.getElementById("error").innerHTML = "";
+						}							
+						else {
+							document.getElementById("currencyDenominations").innerHTML = "";
+							document.getElementById("error").innerHTML = "<emp>Cannot withdraw amount cash is not available! Only Cash Available </emp> <br>  <label> 2000 :- <span>" + o.obj.count_2000 +"</span> </label> <br> <label> 500 :- <span>" + o.obj.count_500 +"</span></label> <br/> <label> 100 :- <span>" + o.obj.count_100 + "</span></label>";
+							$("#amount").focus() ;
+						}
 					}
-					
-                        document.getElementById("currencyDenominations").innerHTML = "<emp>The Denominations for the above amount will be </emp> <br>  <label> 2000 :- <span>" + note2000 +"</span> </label> <br> <label> 500 :- <span>" + note500 +"</span></label> <br/> <label> 100 :- <span>" + note100 + "</span></label>"
-						note2000 = 0, note500 = 0, note100 = 0;
-                        document.getElementById("error").innerHTML = "";
-                    }
-					
-					
-					
-					console.log(this.obj.count_2000, this.obj.count_500 , this.obj.count_100);
-					console.log(leftAmount);
-                }
+				}
+			}
 		  
 		  
 			$( "#btn1" ).on('click',function() {
 				 o.fillATM();
-				$("#btn1").attr("disabled", true);
+				
 				});
-			
 			
 			$( "#btn2" ).on('click',function() {
 					checkError();
 							 
 				});
-			
-			
-            
-            
-								
 		});
 		
 		/*document.onkeydown = function(e) {
